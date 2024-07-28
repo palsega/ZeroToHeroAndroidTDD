c0416c4588be38724e895b41f5772596f6038aa6
Sergey Palchak <spalnavy@gmail.com>
2024-07-28 17:43:51 +0300

    Solve Task008

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,44 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var titleTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        titleTextView = binding.titleTextView
+
+        binding.hideButton.setOnClickListener {
+            titleTextView.visibility = View.INVISIBLE
+        }
+
+        savedInstanceState?.run {
+            titleTextView.visibility = getInt(KEY)
+        }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY, titleTextView.visibility)
+    }
+
+    companion object {
+        private const val KEY = "key"
     }
 }
