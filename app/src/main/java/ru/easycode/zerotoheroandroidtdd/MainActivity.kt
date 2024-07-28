7ece59c61b9c7397828a4ce5643c0f05214e8039
Sergey Palchak <spalnavy@gmail.com>
2024-07-29 00:16:24 +0300

    Solve Task009

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,47 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private var titleTextViewRemoved = false
+    private lateinit var titleTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        titleTextView = binding.titleTextView
+
+        binding.removeButton.setOnClickListener {
+            binding.rootLayout.removeView(titleTextView)
+            titleTextViewRemoved = true
+        }
+
+        savedInstanceState?.run {
+            if (getBoolean(KEY)) {
+                binding.rootLayout.removeView(titleTextView)
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY, titleTextViewRemoved)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val KEY = "key"
     }
 }
