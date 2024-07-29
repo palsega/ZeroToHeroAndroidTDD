8dbe42fc5180a92ba4f3239cb016659d1a44a023
Sergey Palchak <spalnavy@gmail.com>
2024-07-29 12:53:34 +0300

    Solve Task009 using state

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,19 +1,24 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
-    private var titleTextViewRemoved = false
+    private var state: State = State.INITIAL
     private lateinit var titleTextView: TextView
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,20 +26,23 @@ class MainActivity : AppCompatActivity() {
         titleTextView = binding.titleTextView
 
         binding.removeButton.setOnClickListener {
-            binding.rootLayout.removeView(titleTextView)
-            titleTextViewRemoved = true
+            state = State.REMOVED
+            state.removeViewFromLayout(binding.rootLayout, titleTextView)
         }
 
-        savedInstanceState?.run {
-            if (getBoolean(KEY)) {
-                binding.rootLayout.removeView(titleTextView)
+        savedInstanceState?.let { bundle ->
+            state = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                bundle.getSerializable(KEY, State::class.java) as State
+            } else {
+                bundle.getSerializable(KEY) as State
             }
+            state.removeViewFromLayout(binding.rootLayout, titleTextView)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean(KEY, titleTextViewRemoved)
+        outState.putSerializable(KEY, state)
     }
 
     override fun onDestroy() {
@@ -44,5 +52,20 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val KEY = "key"
+    }
+}
+
+interface State : Serializable {
+    fun removeViewFromLayout(viewGroup: ViewGroup, view: View)
+
+    object INITIAL : State {
+        private fun readResolve(): Any = INITIAL
+        override fun removeViewFromLayout(viewGroup: ViewGroup, view: View) = Unit
+    }
+
+    object REMOVED : State {
+        private fun readResolve(): Any = REMOVED
+        override fun removeViewFromLayout(viewGroup: ViewGroup, view: View) =
+            viewGroup.removeView(view)
     }
 }
