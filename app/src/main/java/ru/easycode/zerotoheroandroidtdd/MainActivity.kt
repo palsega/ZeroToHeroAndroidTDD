ee8a68a6f4c8c0751603519f900cf1ff6c39cf2f
Sergey Palchak <spalnavy@gmail.com>
2024-07-29 22:08:56 +0300

    Solve Task011

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,56 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val count = Count.Base(2)
+
+        binding.incrementButton.setOnClickListener {
+            val currentValue = binding.countTextView.text.toString()
+            val newValue = count.increment(currentValue)
+            binding.countTextView.text = newValue
+        }
+
+        savedInstanceState?.run {
+            binding.countTextView.text = getString(KEY)!!
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY, binding.countTextView.text.toString())
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val KEY = "key"
+    }
+}
+
+interface Count {
+    fun increment(number: String): String
+    class Base(private val step: Int) : Count {
+
+        init {
+            if (step <= 0) throw IllegalStateException(
+                "step should be positive, but was $step"
+            )
+        }
+
+        override fun increment(number: String) =
+            (number.toInt() + step).toString()
     }
 }
