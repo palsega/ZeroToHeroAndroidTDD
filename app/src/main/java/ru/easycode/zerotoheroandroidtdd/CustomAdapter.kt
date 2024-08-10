f91148500c425b5efa8ff6601697627eb7fe1a8f
palsega <spalnavy@gmail.com>
2024-08-10 12:56:00 +0300

    Create adapter

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/CustomAdapter.kt
@@ -0,0 +1,26 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.easycode.zerotoheroandroidtdd.databinding.ItemViewBinding
+
+class CustomAdapter(private val inputs: ArrayList<String>) :
+    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+    class ViewHolder(private val binding: ItemViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(text: String) {
+            binding.elementTextView.text = text
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = inputs.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(inputs[position])
+    }
+}
