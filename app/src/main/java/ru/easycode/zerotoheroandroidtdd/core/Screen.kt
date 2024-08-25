75f4d5fef3f84d94dc9b26351cff934c067818d9
palsega <spalnavy@gmail.com>
2024-08-25 11:48:19 +0300

    Add transaction optimization

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/core/Screen.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/core/Screen.kt
@@ -2,6 +2,7 @@ package ru.easycode.zerotoheroandroidtdd.core
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
 
 interface Screen {
 
@@ -9,10 +10,11 @@ interface Screen {
 
     abstract class Replace(private val fragmentClass: Class<out Fragment>) : Screen {
         override fun show(supportFragmentManager: FragmentManager, containerId: Int) {
-            supportFragmentManager.beginTransaction()
-                .replace(containerId, fragmentClass.getDeclaredConstructor().newInstance())
-                .addToBackStack(fragmentClass.simpleName)
-                .commit()
+            supportFragmentManager.commit {
+                replace(containerId, fragmentClass.getDeclaredConstructor().newInstance())
+                setReorderingAllowed(true)
+                addToBackStack(fragmentClass.simpleName)
+            }
         }
     }
 
